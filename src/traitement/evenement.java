2d2c762dc286872f54f8b0687d750d544a501163
HilaryNzamboue <hilarynzamboue@gmail.com>
2023-05-19 16:43:26 +0200

    Merge

--- src/traitement/evenement.java
+++ src/traitement/evenement.java
@@ -3,10 +3,12 @@ package traitement;
 public class evenement {
 	private int type;
 	private double date;
+	private int numero;
 	
 	public evenement(double date,int type) {
 		this.type = type;
 		this.date = date;
+		//this.numero = Math.random() * ( 1 - 0 );
 	}
 	
 	public int getType() {
